df4b8f1ca4becaf3d06c862e05fcd1763ca26ff0
jashika016 <jashikajessy@gmail.com>
2024-11-17 00:35:25 +0530

    Create season_game.py

---
+++ visaa_prep/season_game.py
@@ -0,0 +1,11 @@
+a=int(input())
+if 3<=a<=5:
+    print("Spring")
+elif 6<=a<=8:
+    print("Summer")
+elif 9<=a<=11:
+    print("Autumn")
+elif a==12 or a==1 or a==2:
+    print("Winter")
+else:
+    print("Invalid")
